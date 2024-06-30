802c59735b73f8da0adb66f2038e66d08a09a619
manjulagotur <manjulagotur2@gmail.com>
2024-06-30 22:05:37 +0530

    Update

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1 className='text-4xl  text-center mt-6 font-bold underline bg-slate-500 w-100'>Beyondriffs</h1>
+      <h1 className='text-5xl  text-center mt-6 font-bold underline bg-slate-500 w-100'>Beyondriffs</h1>
       <img
               src="https://cdn.mos.cms.futurecdn.net/jzfgpSpf8Ec25VipkSFEXE.jpg"
               alt=""
