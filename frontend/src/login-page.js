8a7a79965527b3e14c80aad43c149186e21d08f6
kartikay-101 <k.matrix.hacked@gmail.com>
2024-07-02 03:02:58 +0530

    chore: Update login page layout and improve phone number input styling

--- frontend/src/login-page.js
+++ frontend/src/login-page.js
@@ -9,44 +9,53 @@ const Login = () => {
       className="background-image-container flex items-center justify-center min-h-screen bg-cover bg-center"
       style={{ backgroundImage: `url(${bgImage})` }}
       >   
-      <div className="bg-transparent bg-opacity-90 p-8 rounded-lg shadow-custom w-full max-w-md bg-gradient-custom">        
-        <h2 className="text-4xl text-white font-semibold font-Montserrat text-center mb-6 ">Welcome Back</h2>
-        <form>
-        <div className="mb-4">
-          <label className="block text-white font-semibold font-Montserrat text-sm  mb-2" htmlFor="phone">
-          Phone number
-          </label>
-          <div className="flex">
-          
-          <span className="inline-flex font-regular items-center px-3 rounded-l-md border border-r-0 border-gray-100 bg-gray-50 text-gray-500 text-sm"><img src={IndiaFlag} alt="Your Image" className="h-6 w-6" />
-            +91
-          </span>
-          <input
-            type="text"
-            id="phone"
-            className="flex-1 appearance-none border border-gray-300 rounded-r-md w-full py-2 px-3 leading-tight focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent"
-            placeholder="Enter phone number"
-          />
+
+        <div
+          className="rounded-3xl shadow-xl w-full max-w-xl h-auto p-10 backdrop-filter backdrop-blur-lg"
+          style={{ backgroundColor: 'rgb(45 42 57 / 34%)' }}
+        >   
+          <div className="items-center justify-center p-8 bg-transparent">
+          <h2 className="text-4xl text-white font-semibold font-Montserrat text-center mb-6 ">Welcome Back</h2>
+          <form>
+          <div className="mb-4">
+            <label className="block text-white font-semibold font-Montserrat text-sm  mb-2" htmlFor="phone">
+            Phone number
+            </label>
+      
+            <div className="flex">
+            <span className="inline-flex font-regular items-center px-4 rounded-l-md border border-r-0 border-white bg-white text-black-10000 text-sm  "><img src={IndiaFlag} alt="Your Image" className="h-6 w-6" />
+              +91
+              
+            </span>
+            
+            {/* <span className=" border-r bg-white border-black h-6 my-2 "></span> */}
+            <input
+              type="number"
+              id="phone"
+              className="flex-2 appearance-none  rounded-r-md w-full py-3 px-4 leading-tight focus:outline-none focus:ring-1 focus:ring-purple-500 focus:border-transparent"
+              placeholder="Enter phone number"
+            />
+            </div>
           </div>
+          <div className="flex items-center justify-between mb-1 py-5">
+            <button
+            type="button"
+            className="w-full text-white  font-Montserrat py-3 px-4  rounded-lg focus:outline-none focus:shadow-outline gradient-custom inner-shadow text-m"
+            >
+            Send code
+            </button>
+          </div>
+          <div className="flex items-center justify-center mb-6">
+            <img src={googleIcon} alt="Google icon" className="h-8 w-8 " />
+          </div>
+          <div className="flex items-center justify-center">
+          <p style={{ color: 'rgb(179, 175, 179)' }} className="mr-2">Don't have an account? </p>
+            <a href="#" className="text-white font-small font-Montserrat text-sm ">Register</a>
+          </div>
+          </form>
         </div>
-        <div className="flex items-center justify-between mb-1 py-5">
-          <button
-          type="button"
-          className="w-full text-white  font-Montserrat py-3 px-4  rounded-lg focus:outline-none focus:shadow-outline gradient-custom inner-shadow text-m"
-          >
-          Send code
-          </button>
-        </div>
-        <div className="flex items-center justify-center mb-6">
-          <img src={googleIcon} alt="Google icon" className="h-8 w-8 " />
-        </div>
-        <div className="flex items-center justify-center">
-        <p style={{ color: 'rgb(179, 175, 179)' }} className="mr-2">Don't have an account? </p>
-          <a href="#" className="text-white font-small font-Montserrat text-sm ">Register</a>
-        </div>
-        </form>
       </div>
-      </div>
+        </div>
     );
 }
 // Add this line at the end of LoginPage.js
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,14 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      boxShadow: {
-        'custom': '0 4px 24px -2px rgba(0, 0, 0, 0.25)', // Custom shadow
-      },
-      backgroundImage: {
-        'gradient-custom': 'linear-gradient(60% #191A1F, 60% #21222A)', // Custom gradient
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
